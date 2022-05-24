2cd31b32eabe21562bfafa1411dd3e24d9ada92f
eduardosalvarenga <alvarengaseduardo@gmail.com>
2022-05-24 19:55:24 -0300

    Update Principal.java

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -5,17 +5,25 @@ public class Principal {
         String opcao = "";
         Calculadora calculadora = new Calculadora();
         while (!opcao.equals("9")) {
-            opcao = JOptionPane.showInputDialog("1 - Leitura\n2 - Soma \n3 - Subtração " + "\n4 - Produto \n5 - Divisão \n9 - Sair");
+            opcao = JOptionPane.showInputDialog("1 - Leitura\n2 - Soma \n3 - Subtração " + "\n4 - Produto \n5 - Divisão \n9 - Sair")
 
             switch (Integer.parseInt(opcao)) {
-                case 1 -> {
-                    calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor A")));
+                case 1:
+                    calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digie o valor A")));
                     calculadora.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor B")));
-                }
-                case 2 -> JOptionPane.showMessageDialog(null, "Soma: " + calculadora.getAdicao());
-                case 3 -> JOptionPane.showMessageDialog(null, "Subtração: " + calculadora.getSubtracao());
-                case 4 -> JOptionPane.showMessageDialog(null, "Produto: " + calculadora.getProduto());
-                case 5 -> JOptionPane.showMessageDialog(null, "Divisão: " + calculadora.getDivisao());
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Soma: " + calculadora.getAdicao());
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Subtração: " + calculadora.getSubtracao());
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Produto: " + calculadora.getProduto());
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Divisão: " + calculadora.getDivisao());
+                    break;
 
             }
         }
